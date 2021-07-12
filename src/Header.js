bcd651c309117f94f84df112164ffffc1d8f5a7f
brutalism1331 <brutalism.1331@gmail.com>
2021-07-12 11:03:39 -0700

    Completed app setup back to working copy from old repo

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div id="Header">
+        <h1>Horned Beasts</h1>
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/HornedBeast.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './HornedBeast.css';
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>{(this.props.title)}</h2>
+        <img src={this.props.image} alt={this.props.alt}></img>
+        <p>{(this.props.description)}</p>
+      </>
+    )
+  }
+}
+
+export default HornedBeast;
