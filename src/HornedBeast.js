8456f07e408770f99f34292452877f80806146db
brutalism1331 <brutalism.1331@gmail.com>
2021-07-14 12:10:19 -0700

    Saving at a safe point before moving on to next step

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,13 +3,39 @@ import React from 'react';
 import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      numberCog: 0,
+      displayFavorite: false,
+    }
+  };
+
+  addFavorite = () => {
+    this.setState({
+      numberCog: this.state.numberCog + 1,
+    })
+  };
+
+  favoriteDisplayed = () => {
+    this.setState({
+      displayFavorite: true,
+    })
+  };
+
   render() {
     return (
-      <>
+      <article className="hornedBeast">
         <h2>{(this.props.title)}</h2>
-        <img src={this.props.image} alt={this.props.alt}></img>
+        <img src={this.props.image_Url} alt ={this.props.description}></img>
+        <p>{this.state.numberCog} Liked</p>
+        <p>{this.state.displayFavorite ? '🖤' : ''}</p>
         <p>{(this.props.description)}</p>
-      </>
+        <p>{(this.props.keyword)}</p>
+        <p>{(this.props.horns)}</p>
+        <button onClick={this.addFavorite}>favorite</button>
+      </article>
     )
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,35 @@
 import React from 'react';
+// import data from './data.json'
 import HornedBeast from './HornedBeast.js';
 
-
+// data.forEach
 
 class Main extends React.Component {
   render() {
+    // const dataCog = (data) => {
+    //   const objArr = [];
+    //   data.forEach(obj => {
+    //     objArr.push(obj);
+    //   });
+    //   return objArr;
+    // };
+
+    // const itemToRender = <HornedBeast
+    //   title={data.title}
+    //   image_Url={data.image_Url}
+    //   description={data.description}
+    //   keyword={data.keyword}
+    //   horns={data.horns}
+    // />
+
+    // dataCog(data);
+
     return (
       <>
         <HornedBeast
-          title="Rhino Family"
-          alt="Mother (or father) rhino with two babies."
-          image="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-          description="Mother (or father) rhino with two babies."
-        />
-
-        <HornedBeast
-          title="Unicorn Head"
-          alt="Someone wearing a creepy unicorn head mask"
-          image="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-          description="Someone wearing a creepy unicorn head mask!"
+        title={"1"}
+        image_Url={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}
+        description={"TEST 1"}
         />
       </>
     )
